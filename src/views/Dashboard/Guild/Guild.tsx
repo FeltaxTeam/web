0a63721ff356d2e8524a2ae2be052890a7050595
Iron7III <aaronsjimenez@gmail.com>
2022-03-25 06:57:52 +0100

    Added fetching to mongoDB data.

--- src/views/Dashboard/Guild/Guild.tsx
+++ src/views/Dashboard/Guild/Guild.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navigate from '../../.././utility/navigation';
 import { fetchApi } from '../../../utility/fetching';
 import './Guild.css'
@@ -11,19 +11,29 @@ import Loading from '../../Home/Loading/Loading';
 export default function Guild() {
 	let guildId = /[0-9]\w+/.exec(window.location.pathname);
 	let [guild, setGuild] = useState('');
+	let [guildInDb, setGuildInDb] = useState('');
 	useEffect(() => {
 		async function getGuild() {
 			let data: any = await fetchApi(`/discord/guilds/${guildId}`);
 			setGuild(data);
 		}
 		getGuild();
+		async function getGuildInDb() {
+			let data: any = JSON.parse(await fetchApi(`/mongo/getDbGuild/${guildId}`));
+			if (data.error){
+				console.log(`Error fetching the guild in the db: ${data.error} / ${data.message}`);
+				data = null;
+			}
+			setGuildInDb(data);
+		}
+		getGuildInDb();
 	}, []);
 
-	if(guild=='') 
-	return <>
-		<Sidebar guild={guild} />	
-		<Loading />
-	</>;
+	if(guild=='' || guildInDb == '') 
+		return <>
+			<Sidebar guild={guild} />	
+			<Loading />
+		</>;
 
 	return (
 		<>
@@ -34,7 +44,7 @@ export default function Guild() {
 						<Route path="/" element={<Widget guild={guild} />} />
 						<Route path="/premium" element={<Widget guild={guild} />} />
 						<Route path="/panel" element={<Widget guild={guild} />} />
-						<Route path="/settings" element={<Settings />} />
+						<Route path="/settings" element={<Settings guild={guild} dbGuild={guildInDb}/>} />
 						<Route path="*" element={<Navigate to={guildId ? `/dashboard/${guildId}` : `/dashboard`} />} />
 					</Routes>
 				</div>
