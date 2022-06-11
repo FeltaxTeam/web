153cb2c0995a19ce3b4ca7d5d872962268d773ab
Iron7III <aaronsjimenez@gmail.com>
2022-06-11 18:18:49 +0200

    Created.

---
+++ src/views/Acknowledgements/Acknowledgements.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export function Acknowledgements(){
+  return (
+    <React.Fragment>
+      <ol>
+        <li className="ko-fi">
+          https://ko-fi.com/
+          https://more.ko-fi.com/brand-assets
+        </li>
+        <li className="patreon">
+          https://patreon.com/
+        </li>
+        <li className="discord">
+          https://discord.com/
+          https://discord.com/branding
+        </li>
+        <li className="heroku">
+          https://heroku.com/
+          https://brand.heroku.com/
+        </li>
+        <li className="mongodb">
+          https://mongodb.com/
+          https://mongodb.com/brand-resources
+        </li>
+      </ol>
+    </React.Fragment>
+  )
+}
