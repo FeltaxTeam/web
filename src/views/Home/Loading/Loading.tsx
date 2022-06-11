cfdf96a1db39a691c73c6b103aa064a4c959527e
Iron7III <aaronsjimenez@gmail.com>
2022-06-11 18:26:30 +0200

    Improved loading screen.

--- src/views/Home/Loading/Loading.tsx
+++ src/views/Home/Loading/Loading.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter as Router, useRoutes } from 'react-router-dom';
-import './Loading.css';
+import './Loading.scss';
 
 export default function Loading(){
-	return <em>Loading</em>;
+	return <div className='loadingScreen'><span></span></div>;
 };
