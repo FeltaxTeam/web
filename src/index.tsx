354446bc675b31f45fe9b7750b4bf6b418f70ef8
Iron7III <aaronsjimenez@gmail.com>
2022-06-11 18:15:58 +0200

    Improved estructure.

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
