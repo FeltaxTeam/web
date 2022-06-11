5cb106e28cb8a9e522a747911b25548695de012f
Iron7III <aaronsjimenez@gmail.com>
2022-06-11 18:14:44 +0200

    Added subdomains routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Navigate from './utility/navigation';
 import './App.css';
 import Commands from './views/Commands/Commands';
@@ -7,40 +8,85 @@ import Nav from './views/Home/Nav/Nav';
 import Footer from './views/Home/Footer/Footer';
 import NotFoundElement from './views/404/404';
 import Auth from './views/Auth/Auth';
-import Logout from './views/Logout/Logout';
 import Guild from './views/Dashboard/Guild/Guild';
 import Dashboard from './views/Dashboard/Dashboard';
 import Team from './views/Team/Team';
 import Admin from './views/Admin/Admin';
 import Invite from './views/Invite/Invite'
 import Support from './views/Support/Support';
+import AdminAuth from './views/Auth/AdminAuth';
+import { fetchURL } from './utility/fetching';
+import { Acknowledgements } from './views/Acknowledgements/Acknowledgements';
+import TruthOrDare from './views/TruthOrDare/TruthOrDare';
+import CookiesPrompt from './views/CookiesPrompt/CookiesPrompt';
 
 export default function App() {
   let expires = localStorage.getItem('expires');
-  if (expires && new Date(expires).getTime() <= Date.now() + (0*1000*60*10)) localStorage.clear(); //10 minute margin 
-  return (
-    <div className="App">
-      <Router>
-        <Nav paths={['/404', '/team/iron']}/>
-        <Routes>
-          <Route path='*' element={<Navigate to="/404" />} />
-          <Route path='/404' element={<NotFoundElement />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/commands" element={<Navigate to="/commands/main" />} />
-          <Route path="/commands/*" element={<Commands />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/:guildId/*" element={<Guild />} />
-          <Route path="/auth/*" element={<Auth />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/team/*" element={<Team />} />
-          <Route path="/terms" element={<Team />} />
-          <Route path="/privacy" element={<Team />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/invite" element={<Invite />} />
-          <Route path="/support" element={<Support />} />
-        </Routes>
-        <Footer paths={['/', '/team', '/team']}/>
-      </Router>
-    </div>
-  );
+  if (expires && new Date(expires).getTime() <= Date.now() + (0 * 1000 * 60 * 10)) localStorage.clear(); //10 minute margin
+  let [user, setUser] = useState(null);
+  useEffect(() => {
+    async function getUser() {
+      let tokenType = localStorage.getItem('tokenType'), acessToken = localStorage.getItem('accessToken');
+      if ((tokenType == null || acessToken == null) && user != null) {
+        setUser(null);
+      } if ((tokenType != null && acessToken != null) && user == null) {
+        setUser(await fetchURL(tokenType, acessToken, 'https://discord.com/api/users/@me'));
+      }
+    }
+    getUser();
+  }, [user]);
+  //const mainProps = {user: user};
+  const subDomain = window.location.hostname.split('.')[0];
+  switch (subDomain) {
+    case 'admin':
+      return (
+        <Router>
+          <Nav paths={['/404', '/team/iron']} user={user} />
+          <Routes>
+            <Route path='*' element={<Navigate to="/404" />} />
+            <Route path='/404' element={<NotFoundElement />} />
+            <Route path="/auth/*" element={<AdminAuth />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/" element={<Admin user={user} />} />
+          </Routes>
+        </Router>
+      );
+    case 'tod':
+      return (
+        <TruthOrDare />
+      );
+    default:
+      return (
+        <Router>
+          <Nav paths={['/404', '/team/iron']} user={user} />
+          <Routes>
+            <Route path='*' element={<Navigate to="/404" />} />
+            <Route path='/404' element={<NotFoundElement />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/commands" element={<Navigate to="/commands/main" />} />
+            <Route path="/commands/*" element={<Commands />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard/:guildId/*" element={<Guild />} />
+            <Route path="/auth/*" element={<Auth />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/team/*" element={<Team />} />
+            <Route path="/terms" element={<Team />} />
+            <Route path="/privacy" element={<Team />} />
+            <Route path="/admin" element={<Admin user={user} />} />
+            <Route path="/invite" element={<Invite />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/premium" element={<Support />} />
+            <Route path="/acknowledgements" element={<Acknowledgements />} />
+          </Routes>
+          <Footer paths={['/', '/team', '/team']} />
+          <CookiesPrompt />
+        </Router>
+      );
+  }
+
+  function Logout() {
+    localStorage.clear();
+    setUser(null);
+    return <Navigate to="/" />;
+  }
 }
