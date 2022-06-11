4f5ad3d887d904f9eac095b7bbf78165a43700a9
Iron7III <aaronsjimenez@gmail.com>
2022-06-11 18:19:42 +0200

    Improved design and mapping.

--- src/views/Commands/Commands.tsx
+++ src/views/Commands/Commands.tsx
@@ -1,92 +1,78 @@
 import React from 'react';
-import { Route, Routes, Link, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navigate from '../../utility/navigation';
 import './Commands.scss'
-import { commandComponents, commandInfos } from './CommandComponents';
+import { COMMANDS } from './CommandComponents';
+import Sidebar from './Sidebar/Sidebar';
 let path = {
 	get path() { return window.location.href.toLocaleLowerCase(); },
 	get moduleName() { return path.path.split('/').pop()?.split('#')[0]; }
 };
+
 export default class Commands extends React.Component {
 	render() {
 		let moduleName = path.moduleName;
-		let cName = path.path.split('#').pop() ?? 'help';
-		console.log(commandComponents);
-		if (commandComponents[moduleName] == undefined) {
+		//  let cName = path.path.split('#').pop() ?? 'help';
+		console.log(COMMANDS);
+		if (COMMANDS[moduleName] === undefined) {
 			console.log(`moduleName: ${moduleName}`);
 			return <Navigate to="/commands/main" />;
 		}
 		let commandElements = [];
-		for (var i in commandComponents) {
+		for (var i in COMMANDS) {
 			commandElements.push(
-				<Route key={i} path={`/${i}`} element={<>{commandComponents[i].map((el: any) => { return <div className='command'>{el()}</div> })}</>} />
+				<Route key={i} path={`/${i}`} element={
+					<>
+						{
+							COMMANDS[i].map((el, index) => {
+								return <div key={index} className={`command ${i}`}>{
+									<>
+										<div className={'command-focuser'} id={el.name}></div>
+										<h1 className="name">{el.name}</h1>
+										<div className='use'>
+											<div className="usage">
+												<div className="command">
+													<div className="prefix">/</div>
+													<div className="name">{el.name}</div>
+												</div>
+												<div className="options">
+													{
+														el.options.map((option, index) => {
+															return <div key={index} className={`option ${option.optional ? 'optional' : ''}`}>{option.name}</div>
+														})
+													}
+												</div>
+											</div>
+											<ol className="options">
+												{
+														el.options.map((option, index) => {
+															return <li key={index} className={`option ${option.optional ? 'optional' : ''}`}>
+																<div className="name">{option.name}</div>
+																<div className="description">{option.description}</div>
+															</li>
+														})
+													}
+												</ol>		
+										</div>
+										<p>{el.description}</p>
+									</>
+								}</div>
+							})
+						}
+					</>
+				} />
 			);
 		}
 		return (
 			<div className="webBody">
-				<div className="sidebar">
-					<ul className='list'>
-						<CommandList />
-					</ul>
-				</div>
+				<Sidebar />
 				<div className="command-panel">
 					{/* <CommandDisplay/> */}
-					<ComponentFocuser />
 					<Routes>
 						{commandElements}
 					</Routes>
 				</div>
 			</div>
 		);
-	}
-}
-function ComponentFocuser() {
-	const location = useLocation();
-	React.useEffect(() => {
-		let el = document.getElementById(location.hash.substring(1));
-		if (el) el.scrollIntoView();
-	}, [location]);
-	return <></>;
-}
-class CommandList extends React.Component {
-	commandInfos: any;
-	constructor(props: { commandInfos: any }) {
-		super(props);
-		this.commandInfos = props.commandInfos;
-	}
-	render() {
-		const tagIcon: any = { 'premium': 'P', 'slash': '/' };
-		let moduleList = [];
-		for (var i in commandInfos) {
-			moduleList.push(
-				<div className='section' key={i}>
-					<div className='name'>{i}</div>
-					<ul>
-						{
-							commandInfos[i].map((cmd: any) => {
-								return (
-									<Link to={`/commands/${i}#${cmd.name.toLocaleLowerCase()}`} key={`${i}/${cmd.name}`}>
-										<b className="command">
-											<div className="name">{cmd.name}</div>
-											{
-												!cmd.tags ? '' :
-													<div className="features">
-														{
-															cmd.tags.map((tag: string) => {
-																return <div className={tag}>{tagIcon[tag]}</div>
-															})
-														}
-													</div>
-											}
-										</b>
-									</Link>
-								)
-							})
-						}
-					</ul>
-				</div>
-			);
-		}
-		return <>{moduleList}</>;
 	}
 }
